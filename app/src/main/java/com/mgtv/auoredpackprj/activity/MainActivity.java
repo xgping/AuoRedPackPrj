ccf73938ac184fcab0df7e7d462e2f3165a16927
xianggengping <gengping@mgtv.com>
2021-01-14 18:15:29 +0800

    添加首页UI界面

---
+++ app/src/main/java/com/mgtv/auoredpackprj/activity/MainActivity.java
@@ -0,0 +1,98 @@
+package com.mgtv.auoredpackprj.activity;
+
+import android.accessibilityservice.AccessibilityServiceInfo;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.view.View;
+import android.view.accessibility.AccessibilityManager;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.mgtv.auoredpackprj.R;
+
+import java.util.List;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity implements AccessibilityManager.AccessibilityStateChangeListener {
+
+    private TextView pluginStatusText;
+    private ImageView pluginStatusIcon;
+
+    private AccessibilityManager accessibilityManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        pluginStatusText = findViewById(R.id.layout_control_accessibility_text);
+        pluginStatusIcon = findViewById(R.id.layout_control_accessibility_icon);
+
+        //监听AccessibilityService 变化
+        accessibilityManager = (AccessibilityManager) getSystemService(Context.ACCESSIBILITY_SERVICE);
+        accessibilityManager.addAccessibilityStateChangeListener(this);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        updateServiceStatus();
+    }
+
+    @Override
+    protected void onDestroy() {
+        //移除监听服务
+        accessibilityManager.removeAccessibilityStateChangeListener(this);
+        super.onDestroy();
+    }
+
+
+    /**
+     * 更新当前 插件 显示状态
+     */
+    private void updateServiceStatus() {
+        boolean isServiceEnabled = false;
+        List<AccessibilityServiceInfo> accessibilityServices = accessibilityManager.getEnabledAccessibilityServiceList(AccessibilityServiceInfo.FEEDBACK_GENERIC);
+        for (AccessibilityServiceInfo info : accessibilityServices) {
+            if (info.getId().equals(getPackageName() + "/.service.RedPackService")) {
+                isServiceEnabled = true;
+                break;
+            }
+        }
+        if (isServiceEnabled) {
+            pluginStatusText.setText(R.string.service_off);
+            pluginStatusIcon.setBackgroundResource(R.mipmap.ic_stop);
+        } else {
+            pluginStatusText.setText(R.string.service_on);
+            pluginStatusIcon.setBackgroundResource(R.mipmap.ic_start);
+        }
+    }
+
+    public void openAccessibility(View view) {
+        try {
+            Toast.makeText(this, getString(R.string.turn_on_toast) + pluginStatusText.getText(), Toast.LENGTH_SHORT).show();
+            Intent accessibleIntent = new Intent(Settings.ACTION_ACCESSIBILITY_SETTINGS);
+            startActivity(accessibleIntent);
+        } catch (Exception e) {
+            Toast.makeText(this, getString(R.string.turn_on_error_toast), Toast.LENGTH_LONG).show();
+            e.printStackTrace();
+        }
+    }
+
+
+    public void openSettings(View view) {
+        Intent settingsIntent = new Intent(this, SettingsActivity.class);
+        settingsIntent.putExtra("title", getString(R.string.preference));
+        settingsIntent.putExtra("frag_id", "GeneralSettingsFragment");
+        startActivity(settingsIntent);
+    }
+
+    @Override
+    public void onAccessibilityStateChanged(boolean enabled) {
+        updateServiceStatus();
+    }
+}
---
+++ app/src/main/java/com/mgtv/auoredpackprj/utils/ContextProvider.java
@@ -0,0 +1,55 @@
+/**
+ * Copyright 2012 LeTV Technology Co. Ltd., Inc. All rights reserved.
+ *
+ * @Author : qingxia
+ * @Description :
+ */
+
+package com.mgtv.auoredpackprj.utils;
+
+import android.app.Application;
+import android.content.Context;
+
+/**
+ * description: This class provide a global application context.
+ * <p>
+ * author: Created by xianggengping on 2017/10/18.
+ * <p>
+ * email: gengping@mgtv.com
+ */
+public final class ContextProvider {
+    private static Application sApplication;
+    private static Context sContext = null;
+
+    public static Application getApplication() {
+        return sApplication;
+    }
+
+    /**
+     * This function should be invoked in Application while the
+     * application is been
+     * created.
+     * @param application
+     */
+    public static void init(Application application) {
+        if (application == null) {
+            throw new NullPointerException("Can not use null initialized application context");
+        }
+        sContext = application.getApplicationContext();
+        sApplication = application;
+    }
+
+    /**
+     * Get application context.
+     * @return
+     */
+    public static Context getApplicationContext() {
+        if (sContext == null) {
+            throw new NullPointerException("Global application uninitialized");
+        }
+        return sContext;
+    }
+
+    private ContextProvider() {
+    }
+}
